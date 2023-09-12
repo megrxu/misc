c129404a254a27535ec8880ff372db6d56831a3a
Ray <megrxu@proton.me>
2023-09-12 14:55:55 +0800

    [2022] cproduct revise

--- 2022/parallel-cartesian-product/src/lib.rs
+++ 2022/parallel-cartesian-product/src/lib.rs
@@ -1,39 +1,65 @@
 use rayon::iter::plumbing::*;
 use rayon::prelude::*;
-use std::sync::Arc;
+use std::iter::{empty, once};
 
-pub struct CartesianProduct<T: Sized + Send + Clone> {
-    vals: Vec<Arc<Vec<T>>>,
+pub struct CartesianProduct<'a, T> {
+    vals: Vec<&'a [T]>,
     size: usize,
     count: usize,
     lengths: Vec<usize>,
 }
 
-impl<T: Sized + Send + Clone> CartesianProduct<T> {
-    pub fn new(list: &[Arc<Vec<T>>]) -> Self {
-        let lengths = list.iter().map(|l| l.len()).collect::<Vec<usize>>();
-        let size = lengths.iter().product();
+pub struct CartesianProductBuilder<'a, T> {
+    vals: Box<dyn Iterator<Item = &'a [T]> + 'a>,
+    lengths: Box<dyn Iterator<Item = usize>>,
+    size: usize,
+    count: usize,
+}
+
+impl<'a, T> CartesianProductBuilder<'a, T> {
+    pub fn add(mut self, list: &'a [T]) -> CartesianProductBuilder<'a, T> {
+        let l = list.len();
+        assert!(l > 0);
+        self.count += 1;
+        self.size *= l;
+        self.lengths = Box::new(self.lengths.chain(once(l)));
+        self.vals = Box::new(self.vals.chain(once(list)));
+        self
+    }
+
+    pub fn build(self) -> CartesianProduct<'a, T> {
         CartesianProduct {
-            vals: list.to_vec(),
-            count: lengths.len(),
-            size,
-            lengths,
+            vals: self.vals.collect(),
+            count: self.count,
+            size: self.size,
+            lengths: self.lengths.collect(),
+        }
+    }
+}
+
+impl<'a, T> CartesianProduct<'a, T> {
+    pub fn builder() -> CartesianProductBuilder<'a, T> {
+        CartesianProductBuilder {
+            vals: Box::new(empty()),
+            count: 0,
+            size: 1,
+            lengths: Box::new(empty()),
         }
     }
 
-    pub fn nth_unchecked(&self, mut idx: usize) -> Vec<T> {
+    pub fn nth_unchecked(&self, mut idx: usize) -> Vec<&'a T> {
         let mut res = vec![];
         let mut weight = self.size;
         for i in 0..self.count {
             weight /= self.lengths[i];
             let j = idx / weight;
             idx -= j * weight;
-            res.push(self.vals[i][j].clone());
+            res.push(&self.vals[i][j]);
         }
         res
     }
 
-    pub fn nth(&self, idx: usize) -> Option<Vec<T>> {
+    pub fn nth(&self, idx: usize) -> Option<Vec<&'a T>> {
         if idx < self.size {
             Some(self.nth_unchecked(idx))
         } else {
@@ -42,8 +68,8 @@ impl<T: Sized + Send + Clone> CartesianProduct<T> {
     }
 }
 
-impl<T: Sized + Send + Sync + Clone> ParallelIterator for CartesianProduct<T> {
-    type Item = Vec<T>;
+impl<'a, T: Sync> ParallelIterator for CartesianProduct<'a, T> {
+    type Item = Vec<&'a T>;
     fn drive_unindexed<C>(self, consumer: C) -> C::Result
     where
         C: UnindexedConsumer<Self::Item>,
@@ -60,11 +86,17 @@ mod tests {
     use crate::*;
     #[test]
     fn it_works() {
-        let a = vec![0, 1, 2, 3, 4u8];
-        let b = vec![5, 6, 7u8];
-        let c = vec![8, 9u8];
+        let a: Vec<u8> = (0..4).collect();
 
-        let result = CartesianProduct::new(&[Arc::new(a), Arc::new(b), Arc::new(c)]);
-        result.for_each(|i| println!("{:?}", i));
+        let result = CartesianProduct::builder()
+            .add(&a)
+            .add(&a)
+            .add(&a)
+            .add(&a)
+            .add(&a)
+            .add(&a)
+            .build();
+
+        result.for_each(|i| drop(i));
     }
 }
